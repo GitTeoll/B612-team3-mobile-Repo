4e5c18e1f895f0661ee48d809e5413cfbbd9c75d
aahhll654 <aahhll654@gmail.com>
2024-01-07 02:20:39 +0900

    카카오맵 사용 초기 세팅

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:b612_project_team3/common/view/root_tab.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:kakao_map_plugin/kakao_map_plugin.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: 'assets/env/.env');
+
+  /// 라이브러리 메모리에 appKey 등록
+  /// 지도가 호출되기 전에만 세팅해 주면 됩니다.
+  AuthRepository.initialize(appKey: dotenv.env['APP_KEY'] ?? '');
+
   runApp(const _App());
 }
 
