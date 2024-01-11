67777d83e7eb46233612a93e7ff026d43d2fe915
aahhll654 <aahhll654@gmail.com>
2024-01-12 01:57:10 +0900

    오버플로우 해결

--- lib/record/view/record_screen.dart
+++ lib/record/view/record_screen.dart
@@ -8,54 +8,56 @@ class RecordScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const RecentDriveWidget(),
-        SizedBox(
-          width: MediaQuery.of(context).size.width * 0.9,
-          height: MediaQuery.of(context).size.height * 0.02,
-        ),
-        SizedBox(
-          width: MediaQuery.of(context).size.width * 0.9,
-          child: const Text(
-            "Driving Records",
-            style: TextStyle(
-              fontSize: 20,
-            ),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          const RecentDriveWidget(),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.9,
+            height: MediaQuery.of(context).size.height * 0.02,
           ),
-        ),
-        SizedBox(
-          height: MediaQuery.of(context).size.height * 0.015,
-        ),
-        SizedBox(
-          width: MediaQuery.of(context).size.width * 0.05,
-        ),
-        Row(
-          children: [
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.05,
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.95,
-              height: MediaQuery.of(context).size.height * 0.28,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: <Widget>[
-                  const DrivingRecordsCard(),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.07,
-                  ),
-                  const DrivingRecordsCard(),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.07,
-                  ),
-                  const DrivingRecordsCard(),
-                ],
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.9,
+            child: const Text(
+              "Driving Records",
+              style: TextStyle(
+                fontSize: 20,
               ),
             ),
-          ],
-        )
-      ],
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.015,
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.05,
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.05,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.95,
+                height: MediaQuery.of(context).size.height * 0.28,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    const DrivingRecordsCard(),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.07,
+                    ),
+                    const DrivingRecordsCard(),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.07,
+                    ),
+                    const DrivingRecordsCard(),
+                  ],
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
