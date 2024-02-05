aa3285310add69405c92bbbfed3138e18277d67d
aahhll654 <aahhll654@gmail.com>
2024-02-05 17:11:37 +0900

    추가 : 로그인 시 로딩바

--- lib/user/view/login_screen.dart
+++ lib/user/view/login_screen.dart
@@ -4,15 +4,20 @@ import 'package:b612_project_team3/user/provider/user_info_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class LoginScreen extends ConsumerWidget {
+class LoginScreen extends ConsumerStatefulWidget {
   static String get routeName => 'login';
 
   const LoginScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    bool isLoading = false;
+  ConsumerState<LoginScreen> createState() => _LoginScreenState();
+}
 
+class _LoginScreenState extends ConsumerState<LoginScreen> {
+  bool isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
     return DefaultLayout(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -22,18 +27,24 @@ class LoginScreen extends ConsumerWidget {
               if (isLoading) {
                 return;
               }
-              isLoading = true;
+              setState(() {
+                isLoading = true;
+              });
 
               if (await ref.read(userInfoProvider.notifier).login()
                   is UserModelError) {
-                isLoading = false;
+                setState(() {
+                  isLoading = false;
+                });
               }
             },
             child: Center(
-              child: Image.asset(
-                'assets/images/kakao_login/ko/kakao_login_large_narrow.png',
-                width: MediaQuery.of(context).size.width / 2,
-              ),
+              child: isLoading
+                  ? const CircularProgressIndicator()
+                  : Image.asset(
+                      'assets/images/kakao_login/ko/kakao_login_large_narrow.png',
+                      width: MediaQuery.of(context).size.width / 2,
+                    ),
             ),
           ),
         ],
